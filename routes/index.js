a1949cddedfe4383ca26a07dd39b130c7c31e6f1
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-11-09 12:50:55 -0600

    authentication routes in index.js

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const router = express.Router();
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
+const AuthController = require('../controllers/AuthController'); 
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
+
+// Authentication routes
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 
 module.exports = router;
